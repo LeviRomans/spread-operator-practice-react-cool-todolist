006ec5d2a24f8366e2e49b8ab3d22494159aea66
LeviRomans <40896186+LeviRomans@users.noreply.github.com>
2022-03-08 17:43:22 -0500

    add todo list array using state and map operators

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [toDo, setToDo] = useState("");
+
+  function changeHandler(e) {
+    const value = e.target.value;
+    setToDo(value);
+  }
+
+  const [toDoList, setToDoList] = useState([]);
+  console.log(toDoList);
+
+  function clickHandler() {
+    if (toDo) {
+      setToDoList([...toDoList, toDo]);
+      console.log(toDoList);
+      setToDo("");
+    } else {
+      alert("Type something to-do first!");
+    }
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={changeHandler} value={toDo} />
+        <button onClick={clickHandler}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {toDoList.map((x) => (
+            <li>{x}</li>
+          ))}
         </ul>
       </div>
     </div>
   );
 }
-
-export default App;
